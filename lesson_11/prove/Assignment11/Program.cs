ab23d8df06db8506fd42e161db91ae2d0aafbf3e
kcreator5 <kevinwilliamrogers@gmail.com>
2026-04-06 16:48:41 -0600

    commit?

--- lesson_11/prove/Assignment11/Program.cs
+++ lesson_11/prove/Assignment11/Program.cs
@@ -1,4 +1,81 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace assignment11;
+
+public class Assignment11
+{
+    private const long START_NUMBER = 10_000_000_000;
+    private const int RANGE_COUNT = 1_000_000;
+    private const int WORKER_COUNT = 4;
+
+    private static int numbersProcessed = 0;
+    private static int primeCount = 0;
+    private static bool IsPrime(long n)
+    {
+        if (n <= 3) return n > 1;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (long i = 5; i * i <= n; i += 6)
+        {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void Main(string[] args)
+    {
+        var queue = new BlockingCollection<long>(boundedCapacity: 1000);
+
+        var stopwatch = Stopwatch.StartNew();
+
+        // 🔹 Start worker threads
+        Task[] workers = new Task[WORKER_COUNT];
+
+        for (int i = 0; i < WORKER_COUNT; i++)
+        {
+            workers[i] = Task.Run(() =>
+            {
+                foreach (var number in queue.GetConsumingEnumerable())
+                {
+                    Interlocked.Increment(ref numbersProcessed);
+
+                    if (IsPrime(number))
+                    {
+                        Interlocked.Increment(ref primeCount);
+                        Console.Write($"{number}, ");
+                    }
+                }
+            });
+        }
+
+        // 🔹 main thread for adding numbers for the workers to check later
+        for (long i = START_NUMBER; i < START_NUMBER + RANGE_COUNT; i++)
+        {
+            queue.Add(i);
+        }
+
+        // Signals no more stuff
+        queue.CompleteAdding();
+
+        // this waits for workers to finish
+        Task.WaitAll(workers);
+
+        stopwatch.Stop();
+
+        Console.WriteLine("\n");
+        Console.WriteLine($"Numbers processed = {numbersProcessed}");
+        Console.WriteLine($"Primes found      = {primeCount}");
+        Console.WriteLine($"Total time        = {stopwatch.Elapsed}");
+    }
+}
+
+/*
+using System.Diagnostics;
 
 namespace assignment11;
 
@@ -52,3 +129,4 @@ public class Assignment11
         Console.WriteLine($"Total time        = {stopwatch.Elapsed}");        
     }
 }
+*/
--- lesson_11/prove/Assignment11/obj/Debug/net8.0/Assignment11.AssemblyInfo.cs
+++ lesson_11/prove/Assignment11/obj/Debug/net8.0/Assignment11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+454e3c0c6e19d5a89ff74eaef3897c04c698392e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7efc49a2fabf2fb1298905f2924d058652c3a82b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
